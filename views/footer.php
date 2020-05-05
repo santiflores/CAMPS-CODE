cc1311b2623d1210bfcc25816bcd18e5b44298ad
Santi Flores <santiflores@outlook.com>
2020-05-05 00:19:20 -0300

    Mejoras en dropdown, no muchas.

--- views/footer.php
+++ views/footer.php
@@ -2,11 +2,11 @@
     <div class="wrapper_footer">
         <img src="<?php echo RUTA?>/images/logo_camps.png" class="logo_footer">
         <h4>
-            <img src="<?php echo RUTA?>/images/telefono.png" class="footer_icon">
+            <i class="fas fa-phone"></i>
             0381 494-1303
         </h4>
         <h4>
-            <img src="<?php echo RUTA?>/images/ubicacion.png" class="footer_icon">
+            <i class="fas fa-map-marker-alt"></i>
             Av. Rivadavia 877, Alderetes, Tucumán
         </h4>
     </div>
