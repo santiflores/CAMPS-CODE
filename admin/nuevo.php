082474100394afe35de35dc48e623bcb3af31e61
santiflores <santiflores@outlook.com>
2020-06-12 23:14:37 -0300

    Nuevo medico - cargar horarios a base de datos casi listo.

--- admin/nuevo.php
+++ admin/nuevo.php
@@ -15,38 +15,58 @@ if(!$conexion){
 $inicio = '08:00';
 $fin = '20:00';
 
-$fecha_inicio = new DateTime($inicio);
-$fecha_fin = new DateTime($fin);
-$fecha_fin = $fecha_fin->modify('+15 minutes');
+$hora_inicio = new DateTime($inicio);
+$hora_fin = new DateTime($fin);
+$hora_fin = $hora_fin->modify('+30 minutes');
 
-$rango_horarios = new DatePeriod($fecha_inicio, new DateInterval('PT15M'), $fecha_fin);
+$rango_horarios = new DatePeriod($hora_inicio, new DateInterval('PT30M'), $hora_fin);
 
 //Recibir POST
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST'){
-    $nombre = limpiarDatos($_POST['nombre']);
-    $especialidad = limpiarDatos($_POST['especialidad']);
-    $horario = limpiarDatos($_POST['horario']);
-    $dni = $_POST['dni'];
-    $contraseña = $_POST['contraseña'];
-    $horarios_disponibles = $_POST['horario'];
-    $foto = $_FILES['thumb']['tmp_name'];
-    $archivo_subido = '../images' . $_FILES['thumb']['name'];
-    move_uploaded_file($foto, $archivo_subido);
-    $statement = $conexion->prepare(
-        'INSERT INTO `camps`.`medicos`
-        (`nombre`, `especialidad`, `horario de atencion`) 
-        VALUES (:nombre, :especialidad, :horario);' // :dni, :contraseña, :foto * `dni`,`contraseña`, `foto`
-    );
+	$nombre = limpiarDatos($_POST['nombre']);
+	$especialidad = limpiarDatos($_POST['especialidad']);
+	$horario = limpiarDatos($_POST['horario']);
+	$dni = $_POST['dni'];
+	$contraseña = $_POST['contraseña'];
+	
+	$foto = $_FILES['thumb']['tmp_name'];
+	$archivo_subido = '../images' . $_FILES['thumb']['name'];
+	move_uploaded_file($foto, $archivo_subido);
+	
+	$statement = $conexion->prepare(
+		'INSERT INTO `camps`.`medicos`
+		(`nombre`, `especialidad`, `horario de atencion`) 
+		VALUES (:nombre, :especialidad, :horario);' // :dni, :contraseña, :foto * `dni`,`contraseña`, `foto`
+	);
 	$statement->execute(array(
 		':nombre' => $nombre,
 		':especialidad' => $especialidad,
-        ':horario' => $horario,
-        // ':dni' => $dni,
-        // ':contraseña' => $contraseña,
-        // ':foto' => $_FILES['thumb']['name']
+		':horario' => $horario,
+		// ':dni' => $dni,
+		// ':contraseña' => $contraseña,
+		// ':foto' => $_FILES['thumb']['name']
 	));
-    header('Location: ' . RUTA . '/admin/administracion.php');
+	
+	$medico_id = $conexion->query(
+		'SELECT id FROM medicos ORDER BY id DESC LIMIT 1;
+	');
+
+	foreach ($_POST['fila'] as $fila) {
+		$statement = $conexion->prepare(
+			'INSERT INTO `camps` `horarios`
+			(`medico_id`, `dia`, `desde`, `intervalo`,  `hasta`)
+			VALUES(:medico_id, :dia, :desde, :intervalo, :hasta)'
+		);
+		$statement->execute(array(
+			':medico_id' => $medico_id,
+			':dia' => $fila['dia'],
+			':desde' => $fila['desde'],
+			':intervalo' => $fila['intervalo'],
+			':hasta' => $fila['hasta']
+		));
+	}
+	// header('Location: ' . RUTA . '/admin/administracion.php');
 }
 
 require '../views/nuevo.view.php';
--- views/nuevo.view.php
+++ views/nuevo.view.php
@@ -1,43 +1,59 @@
 <!DOCTYPE html>
 <html lang="es" dir="ltr">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Nuevo Medico - CAMPS</title>
-    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="<?php echo RUTA?>/css/stylesheet.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link rel="shortcut icon" type="image.png" href="images/favicon_CAMPS.png">
-    <script src="<?php echo RUTA?>/js/scripts.js"></script>
-    <script src="https://kit.fontawesome.com/aa681c14be.js" crossorigin="anonymous"></script>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Nuevo Medico - CAMPS</title>
+	<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="<?php echo RUTA?>/css/stylesheet.css">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+	<link rel="shortcut icon" type="image.png" href="images/favicon_CAMPS.png">
+	<script src="<?php echo RUTA?>/js/scripts.js"></script>
+	<script src="https://kit.fontawesome.com/aa681c14be.js" crossorigin="anonymous"></script>
   </head>
   <body>
-    <?php require 'header.php'?>
-    <form class="wrapper_agregar_editar" method="post" enctype="multipart/form-data" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF'])?>">
-      <div class="agregar_medico">
-        <div class="agregar_medico_form form">
-          <h5>Agregar medico:</h5>
-          <input type="text" name="nombre" placeholder="Nombre y Apellido">
-          <input type="text" name="especialidad" placeholder="Especialidad">
-          <input type="textarea" name="horario" placeholder="Horario de atencion">
-          <input type="text" name="dni" placeholder="DNI">
-          <input type="password" name="contraseña" placeholder="Contraseña del médico">
-          <input type="file" name="thumb">
-          <input class="submit" type="submit" name="submit" value="Agregar Medico">
-        </div>
-        <div id="nueva_fila">
-          <button type="button" onclick="nuevaFila()">Agregar nueva fila</button>
-          <div class="fila">
-            <h3>Desde</h3>
-            <select name="desde">
-              <?php foreach($rango_horarios as $horario):?>
-                <option value="<?php echo $horario;?>"><?php echo $horario;?></option>
-              <?php endforeach;?>
-            </select>
-          </div>
-        </div>
-      </div>
-    </form>
-    <?php require 'footer.php'?>
+	<?php require 'header.php'?>
+	<form class="wrapper_agregar_editar" method="post" enctype="multipart/form-data" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF'])?>">
+	  <div class="agregar_medico">
+		<div class="agregar_medico_form form">
+			<h5>Agregar medico:</h5>
+			<input type="text" name="nombre" placeholder="Nombre y Apellido">
+			<input type="text" name="especialidad" placeholder="Especialidad">
+			<input type="text" name="horario" placeholder="Horario de atencion">
+			<input type="text" name="dni" placeholder="DNI">
+			<input type="password" name="contraseña" placeholder="Contraseña del médico">
+			<input type="file" name="thumb">
+			<input class="submit" type="submit" name="submit" value="Agregar Medico">
+		</div>
+		<div id="nueva_fila">
+			<button type="button" onclick="nuevaFila()">Agregar nueva fila</button>
+			<select name = 'fila[0][dia]' >
+				<option value="lunes">Lunes</option>
+				<option value="martes">Mates</option>
+				<option value="miercoles">Miercoles</option>
+				<option value="jueves">Jueves</option>
+				<option value="viernes">Viernes</option>
+			</select>
+			<label>Desde</label>
+			<select name="fila[0][desde]" id="">
+				<?php foreach ($rango_horarios as $horario):?>
+					<option value="<?php echo $horario->format('H:i');?>"><?php echo $horario->format('H:i');?></option>
+				<?php endforeach;?>
+		 	</select>
+			<label>Duración del turno:</label>
+			<select name="fila[0][intervalo]">
+				<option value="15">15 Minutos</option>
+				<option value="30">30 minutos</option>
+			</select>
+			<label>Hasta</label>
+			<select name="fila[0][hasta]">
+				<?php foreach ($rango_horarios as $horario):?>
+					<option value="<?php echo $horario->format('H:i');?>"><?php echo $horario->format('H:i');?></option>
+				<?php endforeach;?>
+			</select>
+		</div>
+	  </div>
+	</form>
+	<?php require 'footer.php'?>
   </body>
 </html>
